b91f8c179ba59235b7bb33b485ce4c274c2c6827
Sayantha Hansaka <sayanthahansaka00@gmail.com>
2024-03-07 12:17:33 +0530

    Run the project and print hello world

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <>
+    <h1>Hello sge gyo</h1>
+    <h2>sd</h2>
+    </>
+    
+  );
+}
