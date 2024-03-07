a7f2167f83ff15c6a31bbf05a5f664a5231535c4
Sayantha Hansaka <sayanthahansaka00@gmail.com>
2024-03-07 13:18:17 +0530

    Your commit message here

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,8 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <>
-    <h1>Hello sge gyo</h1>
-    <h2>sd</h2>
+    <h1>Home Page</h1>
     </>
     
   );
