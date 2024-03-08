a992b84074eca4ec1b017e17b0da5bfd18e8e9d4
Sayantha Hansaka <sayanthahansaka00@gmail.com>
2024-03-08 11:57:45 +0530

    Event handling, Creating Funtions and Managing states

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,36 @@
-// import Image from "next/image";
-// import styles from "./page.module.css";
+'use client'
 
-import Button from "./(components)/Button";
+import { useState } from "react";
 
 export default function Home() {
-  
+
+  const [inpuvalue, setvalue] = useState("")
+
+  const hadleClickMe = (yo) => {
+    alert(yo);
+  }
+
+  const hadaleChenge = (e) => {
+    setvalue(e.target.value);
+  }
+
   return (
     <>
-      <h1>Server Component</h1>
-      <Button/>
+      <h3>all Handline Events</h3>
+
+      <input type="text" onChange={hadaleChenge} />
+
+      <button onClick={() => hadleClickMe("yumm yumm")}>Click Me</button>
+     <hr />
+      <br />
+      <h2>Input Valu Her : {inpuvalue}</h2>
+      {
+        inpuvalue == "saya" &&  <div>
+        <h1>Yooow yoooooow</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet, consequuntur? Voluptatibus deserunt ipsum laborum,
+           facere ea ad quo. Accusantium, obcaecati nam? Eos illo numquam harum doloribus quaerat alias incidunt fuga.</p>
+      </div>
+      }
     </>
   );
 }
